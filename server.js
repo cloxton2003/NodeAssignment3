1ca86bbac79bbfa12c47c3b46ae29616a759035f
cloxton2003 <cloxton2003@hotmail.com>
2020-11-28 12:11:28 -0700

    completed assignment with express server

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
 const port = 80;
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, './static')));
 
 
 app.get('/', (request, response) => {
-	response.send('Hello Express :)');
+	response.sendFile(path.join(__dirname, './static/index.html'));
+});
+
+app.get('/about', (request, response) => {
+	response.sendFile(path.join(__dirname, './static/contact.html'));
+});
+
+app.get('/contact', (request, response) => {
+	response.sendFile(path.join(__dirname, './static/contact.html'));
 });
 
 app.listen( port, () => {
